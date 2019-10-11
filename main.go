afd31795954df30e5275372f53e0457d8e2637c6
Shrinivas Kamath <shrkamat@cisco.com>
2019-10-11 18:50:17 +0530

    CRUD mongo

--- main.go
+++ main.go
@@ -3,16 +3,39 @@ package main
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type Mango struct {
+	Id           string `json:"id" bson:"_id,omitempty"`
 	Name         string
 	ShelfLife    int
 	PriceX       int
 	Availability string
 }
+
+type DBCollection struct {
+	coll *mongo.Collection
+}
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.TextFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.TraceLevel)
+}
+
+type Mangoes []Mango
 
 func main() {
 	fmt.Println("Hello, World")
@@ -33,12 +56,94 @@ func main() {
 
 	coll := client.Database("mango-test").Collection("inventory")
 
-	res, err := coll.InsertOne(context.TODO(), Mango{
-		Name:         "Totapuri",
-		ShelfLife:    3,
-		PriceX:       2,
-		Availability: "South India",
-	})
+	mango := Mango{
+		Id:           "PEL~PED~122~langda",
+		Name:         "lagnda",
+		ShelfLife:    6,
+		PriceX:       4,
+		Availability: "North India",
+	}
 
-	fmt.Println("inserted:", res.InsertedID)
+	err = InsertOne(coll, mango)
+
+	logCollection(coll)
+
+	tota := Mango{
+		Id:           "PEL~PED~122~langda",
+		Name:         "lagnda",
+		ShelfLife:    10,
+		PriceX:       8,
+		Availability: "South India",
+	}
+
+	UpdateOne(coll, bson.D{{"_id", "PEL~PED~122~langda"}}, tota)
+
+	logCollection(coll)
+
+	count, _ := DeleteMany(context.TODO(), coll, bson.D{{}})
+
+	fmt.Println("deleted ", count, "items")
+}
+
+func UpdateOne(coll *mongo.Collection, filter interface{}, data interface{}) error {
+
+	res := coll.FindOneAndReplace(context.TODO(), filter, data)
+
+	if err := res.Err(); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func Upsert(coll *mongo.Collection, filter interface{}, data interface{}) error {
+
+	opts := options.FindOneAndReplace().SetUpsert(true)
+
+	res := coll.FindOneAndReplace(context.TODO(), filter, data, opts)
+
+	err := res.Err()
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(res.DecodeBytes())
+		return err
+	}
+
+	bytes, _ := res.DecodeBytes()
+
+	fmt.Println("update", bytes)
+
+	return nil
+}
+
+func DeleteMany(ctx context.Context, coll *mongo.Collection, filter interface{}) (int64, error) {
+	dResult, err := coll.DeleteMany(ctx, filter)
+	if err != nil {
+		fmt.Println("Failed to Del", err)
+		return 0, err
+	}
+	return dResult.DeletedCount, nil
+}
+
+func InsertOne(coll *mongo.Collection, entity interface{}) error {
+	res, err := coll.InsertOne(context.TODO(), entity)
+	if err != nil {
+		fmt.Println("Failed to insert", err)
+	} else {
+		fmt.Println("inserted:", res.InsertedID)
+	}
+	return err
+}
+
+func logCollection(coll *mongo.Collection) {
+	cursor, err := coll.Find(context.TODO(), bson.D{{}})
+	var mangoes Mangoes
+	err = cursor.All(context.TODO(), &mangoes)
+
+	if err != nil {
+		fmt.Println("fetch err", err)
+	}
+
+	fmt.Println(mangoes)
 }
